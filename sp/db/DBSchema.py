de3196313e092c978ba3bce8855be9955d0438ad
SodaPhish <sodaphish@protonmail.ch>
2016-04-30 21:49:27 -0500

    switch to bs4 for DBSchema
    also, may actually have a semi-functional dbschema?

--- sp/db/DBSchema.py
+++ sp/db/DBSchema.py
@@ -4,20 +4,20 @@
 """
 
 import sys, re, os
-from sp.base.Exceptions import DBSchemaError
-from pyparsing import tableName
+from __builtin__ import str
+
 
 try:
-    from xml.dom import minidom
+    from bs4 import BeautifulSoup
 except ImportError:
-    print "couldn't import minidom!"
+    print "couldn't import BeautifulSoup!"
     sys.exit(2)
 
 try:
     from sp.base import Exceptions
     from sp.base import Version
 except:
-    print "Install sp into sys.path()"
+    print "you need to install splib into sys.path()"
     sys.exit(2)
     
     
@@ -29,10 +29,10 @@ class DBSchema():
     
     Schemas look like this: 
         <database>
-        <table name="User">
-            <column name="username" type="text"></column>
-            <column name="password" type="text"></column>
-        </table>
+            <table name="User">
+                <column name="username" type="text"></column>
+                <column name="password" type="text"></column>
+            </table>
         </database>
     
     Supported types are: text, float, int, real, timestamp.
@@ -41,80 +41,86 @@ class DBSchema():
     
     Options for a column are specified in the options="" field within a column definition, e.g. <column name="rowid" options="AUTO_INCREMENT"></column>
     
-    #TODO: add functionality to MySQL and SQLite3 classes to handle database initiation and verification at startup
+    #TODO: add functionality to MySQL and SQLite3 classes to handle database initiation at startup
     
     #TODO: support versions of DBSchema so we can handle old schema definitions appropriately.
     """
     
-    schema_version = Version(0,0,1)
-    schema = None  #the xml schema once loaded
-    type = ""
-    isvalid = False
+    schema_version = Version(0,0,1) #default schema version
+    schema = None  #the soup once its been made
+    isvalid = False #varifies the validity of a schema
 
     
     def __init__(self, schema):
         """
         DBSchema constructor takes a schema definition as one of the following: a string, a filename, or an open file handle.
         """
-        #TODO: populate a schema from string or file
-        r = re.compile('\<.a-zA-Z0-9*.\>')
-        if r.match(schema):
-            # this is a text schema, process it accordingly
-            try:
-                self.schema = minidom.parseString(schema)
+        if type(schema) is str and not os.path.exists( schema ):
+            self.schema = BeautifulSoup(schema, 'html.parser')
+        elif type(schema) is file:
+            self.schema = BeautifulSoup(schema, 'html.parser')
+        elif type(schema) is str and os.path.exists():
+            try: 
+                self.schema = BeautifulSoup(open(schema), 'html.parser')
             except Exception as e:
-                raise DBSchemaError
-        else:
-            # this SHOULD be a file-name or an open file handler being passed in as schema, process it accordingly.
-            if os.path.exists(schema):
-                try:
-                    #try opening the file and reading it in.
-                    self.schema = minidom.parse( schema)
-                except Exception as e:
-                    #something went wrong, go ape-shit!
-                    #TODO: figure out if this was an access error or malformed XML, until then... 
-                    raise DBSchemaError
-
-            else:
-                raise Exceptions.Generic("DBSchema() instantiation method did not receive valid input.")
+                raise Exceptions.FileAccessError
         
         self.isvalid = self.validate()
-        
         if not self.isvalid:
-            raise DBSchemaError
-
-    
+            raise Exceptions.DBSchemaError
+        
+        #set our schema version
+        self.schema_version = self.schema.database['version']
+        
+        
+   
     def validate(self):
         """
         boolean function to verify the validity of a schema
         """
-        #TODO: make sure the schema is complete.  how?
+        #TODO: make sure the schema is complete.  This is going to be much harder than it seems.
         return True
+
+
 
     def get_tables(self):
         """
         return a list of tables in the schema
         """
+        retval = []
+        if self.isvalid:
+            for table in self.schema.findAll("table"):
+                retval.append(table)
+            return retval
+        else:
+            return None
+ 
+ 
     
     def get_table(self,tablename):
         """
         return the rows of a table in the schema
         """
-        #TODO: implement this
-        pass
-
+        retval = []   
+        if self.isvalid:
+            for t in self.schema.findAll("table"):
+                if t['name'] is tablename:
+                    for col in t['name']:
+                        retval.append(col)
+            if len(retval) > 0:
+                return retval
+            else: 
+                return False
+        else:
+            return False
+  
+    
 
     def __repr__(self):
         """
         pretty-print the schema
         """
-        #TODO: clean this up, it will just return a giant string right now, but whatevs.
-        rc = []
-        nodelist = self.schema.getElementsByTagName("database")[0]
-        for node in nodelist:
-            if node.nodeType == node.TEXT_NODE:
-                rc.append(node.data)
-        return ''.join(rc)
+        return self.schema.prettify()
     
     
 #EOF
